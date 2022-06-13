b99227f9e110200911b03de52f01f5a0aaabee2a
taera <taera2@icloud.com>
2022-06-13 11:13:58 +0900

    생성자, getter, setter bs브런치에서 생성

--- gitTest/src/com/gittest/model/vo/Member.java
+++ gitTest/src/com/gittest/model/vo/Member.java
@@ -1,5 +1,20 @@
 package com.gittest.model.vo;
 
 public class Member {
+	
+	private String name;
+	
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
